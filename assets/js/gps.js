919da088056fc4d6e44ff48ae4ba6adbc12a6473
dtstrange <dustintstrange@gmail.com>
2017-11-04 11:32:10 -0700

    added firebase to html and javascript. location data sets to firebase

---
+++ assets/js/gps.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	var config = {
+    apiKey: "AIzaSyDJeCMZnykLSEpaZy27wRZMA-XewrY9NAI",
+    authDomain: "places-1509504215350.firebaseapp.com",
+    databaseURL: "https://places-1509504215350.firebaseio.com",
+    projectId: "places-1509504215350",
+    storageBucket: "places-1509504215350.appspot.com",
+    messagingSenderId: "506073607799"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+	$("#locate").on("click", function(){
+		axios.post("https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDxqJutbSvsf280kK98GODy0fkmj9zRG0E ")
+	
+		.then(function(response){
+			console.log(response);
+			var location = response.data.location;
+
+			database.ref().set({
+			location: location
+		  })
+		})
+	});
+ });
--- assets/js/javascript.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$("#locate").on("click", function(){
-		axios.post("https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDxqJutbSvsf280kK98GODy0fkmj9zRG0E ")
-	
-		.then(function(response){
-			console.log(response)
-		})
-	});
- });
